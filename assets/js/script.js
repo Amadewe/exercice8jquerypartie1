29d4bac518fd7549d1f4844e8acd21d538fdc824
Amadewe <dewevre.amandine@gmail.com>
2019-02-01 19:00:13 +0100

    Ajout d'une seconde version avec la méthode .children()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 // Donner à tous les éléments li enfants de ol la couleur rouge.
+// $(function() {
+//   $('ol').css('color', 'red');
+// });
+
 $(function() {
-  $('ol').css('color', 'red');
+$('ol').children('li').css('color', 'red');
 });
